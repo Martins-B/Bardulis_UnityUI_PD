73a6576849ffe72743f66fd9897b36086b1961e7
Martins-B <[martinsbardulis@gmail.com]>
2021-03-16 22:34:31 +0200

    Izveidoti ievades lauki

---
+++ Assets/Skripti/VardaParadisana.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VardaParadisana : MonoBehaviour{
+    //Mainīgajā saglabās rekstu ko raksta teksta laukā
+    public string teksts,vecums;
+       //Lauks, kurā ieraksta tekstu
+    public GameObject ievadesLauks, ievadesLauks2;
+    //Kur attēlot tekstu
+    public GameObject tekstaAttelosana;
+
+    //Funkcija tiek izsaukta nospiežot pogu
+    public void uzglabatTekstu()
+    {
+        teksts = ievadesLauks.GetComponent<Text>().text;
+        vecums = ievadesLauks2.GetComponent<Text>().text;
+        tekstaAttelosana.GetComponent<Text>().text = teksts+" ir "+vecums+" gadus vecs!";
+    }
+}
